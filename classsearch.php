c2da5b7e9fc5bbc40b6f480ae57cae49be9ca6f5
Colin Steifel <csteifel@gmail.com>
2012-10-24 19:06:41 -0400

    Fixed json to be valid, changed some database formatting

--- classsearch.php
+++ classsearch.php
@@ -9,12 +9,12 @@
 
         if(@trim($_GET['query']) != ""){
                 $_GET['query'] = $mysql->escapeString(trim($_GET['query']));
-                $results = $mysql->query("SELECT id, crn, coursenumber, name, credithours FROM classes WHERE name LIKE '%{$_GET['query']}%' OR coursenumber LIKE '%{$_GET['query']}%';");
+                $results = $mysql->query("SELECT id, crn, school, coursenumber, section, name, credithours FROM classes WHERE name LIKE '%{$_GET['query']}%' OR coursemeta LIKE '%{$_GET['query']}%';");
 
                 for($i = 0; $i < count($results); $i++){
                         if(@$_GET['ajax']==1){
                                 //No need to run through templating responding with json
-                                $variables[] = array('id' => $results[$i]['id'], 'crn' => $results[$i]['crn'], 'coursenumber' => $results[$i]['coursenumber'], 'name' => $results[$i]['name'], 'credits' => $results[$i]['credithours']);
+                                $variables[] = array('id' => $results[$i]['id'], 'crn' => $results[$i]['crn'], 'school' => $results[$i]['school'], 'coursenumber' => $results[$i]['coursenumber'], 'section' => $results[$i]['section'], 'name' => $results[$i]['name'], 'credits' => $results[$i]['credithours']);
                         }else{
                                 $variables['results'] .= template("searchresult", $results[$i]);
                         }
@@ -24,10 +24,10 @@
 
         //Ajax response
         if(@$_GET['ajax']){
-                echo "{".json_encode($variables)."}";
+                echo "{\"data\":".json_encode($variables)."}";
                 die();
         }
         //Set up and display the page
-        echo template("classsearch", $variables);
-        
+        $content = template("classsearch", $variables);
+        echo template("index", array('maincontent' => $content));
 ?>
--- template/searchresult.tpl
+++ template/searchresult.tpl
@@ -1,5 +1,5 @@
 <div>
-        CRN: {$ss_crn} Course number: {$ss_coursenumber} <br />
+        CRN: {$ss_crn} Course number: {$ss_school}-{$ss_coursenumber}-{$ss_section} <br />
         {$ss_name} Credit hours: {$ss_credithours}
 </div>
 <br />
