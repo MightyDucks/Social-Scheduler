6095bc24f5f4fe93f44924bf217e949671b06dc4
Colin Steifel <csteifel@gmail.com>
2012-10-23 20:16:04 -0400

    Added ajax/json response for class searching

--- classsearch.php
+++ classsearch.php
@@ -11,13 +11,22 @@
                 $_GET['query'] = $mysql->escapeString(trim($_GET['query']));
                 $results = $mysql->query("SELECT id, crn, coursenumber, name, credithours FROM classes WHERE name LIKE '%{$_GET['query']}%' OR coursenumber LIKE '%{$_GET['query']}%';");
 
-
                 for($i = 0; $i < count($results); $i++){
-                        $variables['results'] .= template("searchresult", $results[$i]);
+                        if(@$_GET['ajax']==1){
+                                //No need to run through templating responding with json
+                                $variables[] = array('id' => $results[$i]['id'], 'crn' => $results[$i]['crn'], 'coursenumber' => $results[$i]['coursenumber'], 'name' => $results[$i]['name'], 'credits' => $results[$i]['credithours']);
+                        }else{
+                                $variables['results'] .= template("searchresult", $results[$i]);
+                        }
                 }
         }
         
 
+        //Ajax response
+        if(@$_GET['ajax']){
+                echo "{".json_encode($variables)."}";
+                die();
+        }
         //Set up and display the page
         echo template("classsearch", $variables);
         
--- template/classsearch.tpl
+++ template/classsearch.tpl
@@ -3,6 +3,9 @@
         <title>Class search</title>
 </head>
 <body>
+        <form action='classsearch.php' method='get'>
+                <input type='text' name='query' /> <input type='submit' value='Search' />
+        </form>
         {$ss_results}
 </body>
 </html>
